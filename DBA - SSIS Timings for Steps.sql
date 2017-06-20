5bb1657a6548e5b33428c3966106076fed6faec6
Raymond Betts <raybetts@rfbc.net>
2017-06-20 13:50:06 +0100

    Add files via upload
    Added group by statement

--- DBA - SSIS Timings for Steps.sql
+++ DBA - SSIS Timings for Steps.sql
@@ -1,26 +1,49 @@
 USE SSISDB;
 
 -- User for @SSIS
-SELECT	DISTINCT project_name
-FROM	SSISDB.[catalog].executions;
+--SELECT	DISTINCT project_name
+--FROM	SSISDB.[catalog].executions;
 
-DECLARE @DATE DATE			= GETDATE() -2
+DECLARE @DATE DATE			= GETDATE()
 ,		@SSIS VARCHAR(500)	= 'satsuma.etl';
+
+
 
 SELECT  e.folder_name
 ,		e.project_name
 ,		e.package_name
-,		es.execution_path
+,		SUBSTRING(es.execution_path, LEN(e.package_name) - 3, LEN(es.execution_path))	AS ExecutionPath
+,		MAX(DATEDIFF(minute, es.start_time, es.end_time))								AS MAXexecution_time
+,		AVG(DATEDIFF(minute, es.start_time, es.end_time))								AS AVGexecution_time
+FROM	SSISDB.[catalog].executions				e
+JOIN	SSISDB.[catalog].executable_statistics	es	ON e.execution_id = es.execution_id
+WHERE	1 = 1
+AND		e.project_name	= @SSIS
+--AND		es.execution_path LIKE '%\Warehouse MasterPackage Satsuma\Root\SEQC - warehouse3%'
+AND		es.execution_path LIKE '%\Warehouse MasterPackage Satsuma\Root\SEQC - Warehouse3%'
+GROUP BY	e.folder_name
+,		e.project_name
+,		e.package_name
+,		SUBSTRING(es.execution_path, LEN(e.package_name) - 3, LEN(es.execution_path))
+ORDER BY MAXexecution_time DESC;
+
+/*
+
+SELECT  e.folder_name
+,		e.project_name
+,		e.package_name
+,		SUBSTRING(es.execution_path, LEN(e.package_name) - 3, LEN(es.execution_path)) AS ExecutionPath
 ,		DATEDIFF(minute, es.start_time, es.end_time) AS execution_time
 FROM	SSISDB.[catalog].executions				e
 JOIN	SSISDB.[catalog].executable_statistics	es	ON e.execution_id = es.execution_id
 WHERE	e.start_time	>= @DATE
 AND		e.project_name	= @SSIS
+--AND		es.execution_path LIKE '%\Warehouse MasterPackage Satsuma\Root\SEQC - warehouse3%'
+AND		es.execution_path LIKE '%\Interface MasterPackage Satsuma\Root\SEQC - Interface2%'
 ORDER BY execution_time DESC;
 
 
-
-SELECT     CAST(MSG.message_time AS datetime)	AS message_time
+SELECT		OPR.object_name
 ,			CASE message_source_type
                 WHEN 10 THEN 'Entry APIs, such as T-SQL and CLR Stored procedures'
                 WHEN 20 THEN 'External process used to run package (ISServerExec.exe)'
@@ -30,19 +53,16 @@ SELECT     CAST(MSG.message_time AS datetime)	AS message_time
                 WHEN 60 THEN 'Data Flow task'
             END												AS message_source_type
 ,			CAST(start_time AS datetime)					AS start_time
-,			OPR.object_name
 ,			message
 ,			LEFT(message, CHARINDEX(':', message) -1)		AS Block
 ,			CONVERT(TIME(0), LEFT(RIGHT(message, 13),12))	AS execution_time
 FROM        SSISDB.[catalog].operation_messages	MSG
 JOIN		SSISDB.[catalog].operations			OPR	ON OPR.operation_id = MSG.operation_id
 WHERE		start_time			> @DATE 
-and			message				like '%:Finish%' 
-and			message				not like 'INSERT Record SQL Task:Finished%'
-and			message_source_type <> 30
-and			message_source_type <> 50
-and			OPR.object_name		= @SSIS
---ORDER BY message_time DESC
+AND			message				LIKE '%:Finish%' 
+AND			message				NOT LIKE 'INSERT Record SQL Task:Finished%'
+AND			message_source_type NOT IN (30, 50)
+AND			OPR.object_name		= @SSIS
 ORDER BY execution_time DESC
 
-
+*/
