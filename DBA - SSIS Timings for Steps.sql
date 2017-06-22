426701493ad82f071c051518807c6b78a6a638b1
Raymond Betts <raybetts@rfbc.net>
2017-06-22 08:57:02 +0100

    Add files via upload

--- DBA - SSIS Timings for Steps.sql
+++ DBA - SSIS Timings for Steps.sql
@@ -4,30 +4,30 @@ USE SSISDB;
 --SELECT	DISTINCT project_name
 --FROM	SSISDB.[catalog].executions;
 
-DECLARE @DATE DATE			= GETDATE()
-,		@SSIS VARCHAR(500)	= 'satsuma.etl';
+DECLARE @DATE		DATE			= GETDATE()
+,		@SSIS		VARCHAR(500)	= 'satsuma.etl'
+,		@ExecPath	VARCHAR(1000)	= 'Warehouse_FactAgreementTransaction';
 
 
 
-SELECT  e.folder_name
-,		e.project_name
-,		e.package_name
-,		SUBSTRING(es.execution_path, LEN(e.package_name) - 3, LEN(es.execution_path))	AS ExecutionPath
-,		MAX(DATEDIFF(minute, es.start_time, es.end_time))								AS MAXexecution_time
-,		AVG(DATEDIFF(minute, es.start_time, es.end_time))								AS AVGexecution_time
-FROM	SSISDB.[catalog].executions				e
-JOIN	SSISDB.[catalog].executable_statistics	es	ON e.execution_id = es.execution_id
-WHERE	1 = 1
-AND		e.project_name	= @SSIS
---AND		es.execution_path LIKE '%\Warehouse MasterPackage Satsuma\Root\SEQC - warehouse3%'
-AND		es.execution_path LIKE '%\Warehouse MasterPackage Satsuma\Root\SEQC - Warehouse3%'
+SELECT		e.folder_name
+,			e.project_name
+,			e.package_name
+,			SUBSTRING(es.execution_path, LEN(e.package_name) - 3, LEN(es.execution_path))	AS ExecutionPath
+,			MAX(DATEDIFF(minute, es.start_time, es.end_time))								AS MAXexecution_time
+,			AVG(DATEDIFF(minute, es.start_time, es.end_time))								AS AVGexecution_time
+FROM		SSISDB.[catalog].executions				e
+JOIN		SSISDB.[catalog].executable_statistics	es	ON e.execution_id = es.execution_id
+WHERE		1 = 1
+AND			e.project_name		= @SSIS
+AND			es.execution_path	LIKE '%' + @ExecPath + '%'
 GROUP BY	e.folder_name
-,		e.project_name
-,		e.package_name
-,		SUBSTRING(es.execution_path, LEN(e.package_name) - 3, LEN(es.execution_path))
-ORDER BY MAXexecution_time DESC;
+,			e.project_name
+,			e.package_name
+,			SUBSTRING(es.execution_path, LEN(e.package_name) - 3, LEN(es.execution_path))
+ORDER BY	MAXexecution_time DESC;
 
-/*
+
 
 SELECT  e.folder_name
 ,		e.project_name
@@ -36,12 +36,11 @@ SELECT  e.folder_name
 ,		DATEDIFF(minute, es.start_time, es.end_time) AS execution_time
 FROM	SSISDB.[catalog].executions				e
 JOIN	SSISDB.[catalog].executable_statistics	es	ON e.execution_id = es.execution_id
-WHERE	e.start_time	>= @DATE
-AND		e.project_name	= @SSIS
---AND		es.execution_path LIKE '%\Warehouse MasterPackage Satsuma\Root\SEQC - warehouse3%'
-AND		es.execution_path LIKE '%\Interface MasterPackage Satsuma\Root\SEQC - Interface2%'
+WHERE	e.start_time		>= @DATE
+AND		e.project_name		= @SSIS
+AND		es.execution_path	LIKE '%' + @ExecPath + '%'
 ORDER BY execution_time DESC;
-
+/*
 
 SELECT		OPR.object_name
 ,			CASE message_source_type
