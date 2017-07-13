66d108b16a9647c302f49e4547a548fb75299efe
RayBetts <raymond.betts1.@provident.co.uk>
2017-07-13 10:14:15 +0100

    Added more calcs on averages etc

--- DBACode/SQL Agent/Job Step Timings.sql
+++ DBACode/SQL Agent/Job Step Timings.sql
@@ -27,12 +27,18 @@ ORDER BY	JobName
 SELECT		j.name									AS JobName
 ,			s.step_id								AS Step
 ,			s.step_name								AS StepName
-,			AVG(((run_duration / 10000 * 3600 
-				+ (run_duration / 100) % 100 * 60 
-				+ run_duration %100 + 31 ) / 60))	AS AvgRunDurationMinutes
-,			MAX(((run_duration / 10000 * 3600 
-				+ (run_duration / 100) % 100 * 60 
-				+ run_duration %100 + 31 ) / 60))	AS MaxRunDurationMinutes
+,			MIN((h.run_duration / 10000 * 3600 + 
+				(run_duration / 100) % 100 * 60 + 
+				run_duration%100 + 31 ) / 60)		AS LowestMin
+,			AVG((h.run_duration / 10000 * 3600 + 
+				(run_duration / 100) % 100 * 60 + 
+				run_duration % 100 + 31 ) / 60)		AS AverageMin
+,			MAX((h.run_duration / 10000 * 3600 + 
+				(run_duration / 100) % 100 * 60 + 
+				run_duration % 100 + 31 ) / 60)		AS HighestMin
+,			CONVERT(DECIMAL(5,2), STDEV((h.run_duration / 10000 * 3600 + 
+				(run_duration / 100) % 100 * 60 + 
+				run_duration%100 + 31 ) / 60))		AS stdevMin
 FROM		msdb.dbo.sysjobs		j 
 JOIN		msdb.dbo.sysjobsteps	s	ON j.job_id		= s.job_id
 JOIN		msdb.dbo.sysjobhistory	h	ON s.job_id		= h.job_id 
