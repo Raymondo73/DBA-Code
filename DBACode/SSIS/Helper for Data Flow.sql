d9a72dc7e07f80b853dab85eb1608836c90b1bd3
Raymond <raybetts@rfbc.net>
2017-09-22 23:27:21 +0100

    extra bits of code

--- DBACode/SSIS/Helper for Data Flow.sql
+++ DBACode/SSIS/Helper for Data Flow.sql
@@ -22,4 +22,18 @@ SET @RowSize100 = (@Buffer100 / @RowSize);
 SELECT 'Rows for 100MB Buffer ' +  CONVERT(VARCHAR, @RowSize100); -- DefaultBufferMaxRows 
 
 
+DECLARE @BufferSize INT				= 10485760
+,		@TableName	VARCHAR(200)	= 'Table'
+,		@SchemaName VARCHAR(50)		= 'Schema';
 
+WITH RowByteSize AS
+(
+SELECT	SUM(max_length) AS	RowLength
+FROM	sys.tables	t
+JOIN	sys.columns c	ON t.object_id = c.object_id
+JOIN	sys.schemas s	ON t.schema_id = s.schema_id
+WHERE	t.name = @TableName
+AND		s.name = @SchemaName
+)
+SELECT	@BufferSize / RowLength AS DefaultBufferRows
+FROM	RowByteSize;
