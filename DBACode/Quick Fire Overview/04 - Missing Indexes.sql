1aca7b289acc9e55027bf2ebf5963e410a0014d0
Ray <raybetts@rfbc.net>
2025-09-30 16:28:32 +0100

    updated

--- DBACode/Quick Fire Overview/04 - Missing Indexes.sql
+++ DBACode/Quick Fire Overview/04 - Missing Indexes.sql
@@ -10,4 +10,5 @@ SELECT		migs.avg_total_user_cost * (migs.avg_user_impact * 0.01) * (migs.user_se
 FROM		sys.dm_db_missing_index_group_stats		migs
 JOIN		sys.dm_db_missing_index_groups			mig ON migs.group_handle = mig.index_group_handle
 JOIN		sys.dm_db_missing_index_details			mid ON mig.index_handle = mid.index_handle
+WHERE		mid.database_id > 4                  -- exclude system DBs
 ORDER BY	ImprovementMeasure DESC;
