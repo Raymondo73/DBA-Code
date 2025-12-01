c447f0b249aeb54db734717caf9e08accd3d127a
Ray <raybetts@rfbc.net>
2025-12-01 11:52:01 +0000

    added 2 dp to query and tidy up

--- DBACode/Misc/DB - Overview.sql
+++ DBACode/Misc/DB - Overview.sql
@@ -1,40 +1,34 @@
 CREATE TABLE #FileSize
 (
-    dbName              NVARCHAR(128)
+    DBName              NVARCHAR(128)
 ,   FileName            NVARCHAR(128)
 ,   PhysicalName        NVARCHAR(128)
 ,   type_desc           NVARCHAR(128)
 ,   recovery_model_desc NVARCHAR(128)
-,   CurrentSizeMB       DECIMAL(10,2)
-,   FreeSpaceMB         DECIMAL(10,2)
+,   CurrentSizeMB       VARCHAR(10)
+,   FreeSpaceMB         VARCHAR(10)
 );
     
 INSERT INTO #FileSize
             (dbName, FileName, PhysicalName, type_desc, recovery_model_desc, CurrentSizeMB, FreeSpaceMB)
 EXEC        sp_msforeachdb 
             'use [?]; 
-             SELECT DB_NAME()               AS DbName
-             ,      df.name                 AS FileName
-             ,      df.physical_name        AS PhysicalName
+             SELECT DB_NAME()                       AS DbName
+             ,      df.name                         AS FileName
+             ,      df.physical_name                AS PhysicalName
              ,      df.type_desc
              ,      d.recovery_model_desc
-             ,      df.size / 128.0         AS CurrentSizeMB
-             ,      df.size / 128.0 - CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS INT) / 128.0 AS FreeSpaceMB
+             ,      FORMAT(df.size / 128.0, ''N2'') AS CurrentSizeMB
+             ,      FORMAT(df.size / 128.0 - CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS INT) / 128.0, ''N2'') AS FreeSpaceMB
              FROM   sys.database_files df
              JOIN   sys.databases      d   ON d.name = DB_NAME()
-             WHERE  type IN (0,1)
+             WHERE  type IN (0, 1)
              ';
     
-SELECT  * 
-FROM    #FileSize
+SELECT      * 
+FROM        #FileSize
+ORDER by    DBName;
 --ORDER BY LEFT(PhysicalName, 1)
---WHERE   dbName NOT IN ('distribution', 'master', 'model', 'msdb');
     
 DROP TABLE #FileSize;
-
-
-/* 
-select * from sys.database_files
-select * from sys.databases
-*/
 
