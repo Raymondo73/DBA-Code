e703c8607c7f360e41ef6022332e062793f8e929
Ray <raybetts@rfbc.net>
2025-12-04 11:50:59 +0000

    tidy up

--- DBACode/Security/SA - Disable Checks.sql
+++ DBACode/Security/SA - Disable Checks.sql
@@ -17,6 +17,7 @@ JOIN	msdb.dbo.sysjobsteps	sjs ON sj.job_id = sjs.job_id
 WHERE	sjs.command		LIKE '%sa%' 
 OR		sjs.proxy_id	= 0;
 
+
 -- WE have another sysadmin account. If we don't we can't disable!
 SELECT	name
 ,		type_desc
@@ -24,12 +25,14 @@ SELECT	name
 FROM	sys.server_principals
 WHERE	IS_SRVROLEMEMBER('sysadmin', name) = 1;
 
+
 -- SQL Server Auth Mode. Windows only, trivial to disable, mixed - double check everything
 -- 2 = Windows, 1 or 0 = SQL
 EXEC xp_instance_regread 
-    N'HKEY_LOCAL_MACHINE',
-    N'Software\Microsoft\MSSQLServer\MSSQLServer',
-    N'LoginMode';
+        N'HKEY_LOCAL_MACHINE'
+    ,   N'Software\Microsoft\MSSQLServer\MSSQLServer'
+    ,   N'LoginMode';
+
 
 -- Hardcoded References in SSIS, Linked Server etc
 SELECT  name
@@ -38,5 +41,4 @@ SELECT  name
 FROM    sys.servers         s
 JOIN    sys.linked_logins   ll ON s.server_id = ll.server_id
 WHERE   ll.remote_name = 'sa';
-
 
